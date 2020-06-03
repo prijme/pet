55b13c8543b9ea7aa8ddce00209aa58003dd44cb
paulrjmsmits <paulrjmsmits@users.noreply.github.com>
2020-06-03 03:47:32 +0200

    navigation added

--- cms/models.py
+++ cms/models.py
@@ -1,17 +1,21 @@
 from .blocks import InlineImageBlock, InlineVideoBlock
 from django import forms
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
-from modelcluster.fields import ParentalManyToManyField
+from django_extensions.db.fields import AutoSlugField
+from modelcluster.fields import ParentalManyToManyField, ParentalKey
+from modelcluster.models import ClusterableModel
 from wagtail.core import blocks
 from wagtail.core.models import Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, PageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, PageChooserPanel, InlinePanel
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
 from wagtailcodeblock.blocks import CodeBlock
-from wagtailtrans.models import TranslatablePage
+from wagtailtrans.models import TranslatablePage, Language
 
 class HomePage(TranslatablePage):
     intro = RichTextField(blank=True)
@@ -147,3 +151,84 @@ class ArticleIndexPage(TranslatablePage):
     content_panels = TranslatablePage.content_panels + [
         FieldPanel('intro', classname='full'),
     ]
+
+
+@register_snippet
+class Menu(ClusterableModel):
+
+    title = models.CharField(max_length=50)
+    slug = AutoSlugField(populate_from='title', editable=True, help_text="Unique identifier of menu. Will be populated automatically from title of menu. Change only if needed.")
+
+    panels = [
+        MultiFieldPanel([
+            FieldPanel('title'),
+            FieldPanel('slug'),
+        ], heading=_("Menu")),
+        InlinePanel('menu_items', label=_("Menu Item"))
+    ]
+
+    def __str__(self):
+        return self.title
+
+
+class MenuItem(Orderable):
+    menu = ParentalKey('Menu', related_name='menu_items', help_text=_("Menu to which this item belongs"))
+    title = models.CharField(max_length=50, help_text=_("Title of menu item that will be displayed"))
+    link_url = models.CharField(max_length=500, blank=True, null=True, help_text=_("URL to link to, e.g. /accounts/signup (no language prefix, LEAVE BLANK if you want to link to a page instead of a URL)"))
+    link_page = models.ForeignKey(
+        TranslatablePage, blank=True, null=True, related_name='+', on_delete=models.CASCADE, help_text=_("Page to link to (LEAVE BLANK if you want to link to a URL instead)"),
+    )
+    title_of_submenu = models.CharField(
+        blank=True, null=True, max_length=50, help_text=_("Title of submenu (LEAVE BLANK if there is no custom submenu)")
+    )
+    icon = models.ForeignKey(
+        'wagtailimages.Image', blank=True, null=True, on_delete=models.SET_NULL, related_name='+',
+    )
+    show_when = models.CharField(
+        max_length=15,
+        choices=[('always', _("Always")), ('logged_in', _("When logged in")), ('not_logged_in', _("When not logged in"))],
+        default='always',
+    )
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('link_url'),
+        PageChooserPanel('link_page'),
+        FieldPanel('title_of_submenu'),
+        ImageChooserPanel('icon'),
+        FieldPanel('show_when'),
+    ]
+
+    def trans_page(self, language_code):
+        if self.link_page:
+            can_page = self.link_page if self.link_page.is_canonical else self.link_page.canonical_page
+            if language_code == settings.LANGUAGE_CODE: # requested language is the canonical language
+                return can_page
+            try:
+                language = Language.objects.get(code=language_code)
+            except Language.DoesNotExist: # no language found, return original page
+                return self.link_page
+            return TranslatablePage.objects.get(language=language, canonical_page=can_page)
+        return None
+
+    def trans_url(self, language_code):
+        if self.link_url:
+            return '/' + language_code + self.link_url
+        elif self.link_page:
+            return self.trans_page(language_code).url
+        return None
+
+    @property
+    def slug_of_submenu(self):
+        # becomes slug of submenu if there is one, otherwise None
+        if self.title_of_submenu:
+            return slugify(self.title_of_submenu)
+        return None
+
+    def show(self, authenticated):
+        return ((self.show_when == 'always')
+                or (self.show_when == 'logged_in' and authenticated)
+                or (self.show_when == 'not_logged_in' and not authenticated))
+
+    def __str__(self):
+        return self.title
---
+++ cms/templatetags/cms_tags.py
@@ -0,0 +1,38 @@
+from cms.models import Menu
+from django import template
+from django.utils import translation
+
+register = template.Library()
+
+@register.simple_tag()
+def get_menu(slug, page, logged_in):
+    # returns a list of dicts with title, url, slug, page and icon of all items in the menu of the given slug or page
+
+    try:
+        # see if there is a custom menu defined for the slug of the item
+        candidates = Menu.objects.get(slug=slug).menu_items.all()
+        language_code = translation.get_language()
+
+        # create a list of all items that should be shown in the menu depending on logged_in
+        menu_items = []
+        for candidate in candidates:
+            if candidate.show(logged_in):
+                menu_items.append({'title': candidate.title, 'url': candidate.trans_url(language_code),
+                                   'slug': candidate.slug_of_submenu, 'page': candidate.trans_page(language_code), 'icon': candidate.icon})
+        return menu_items
+    except Menu.DoesNotExist:
+        pass
+    try:
+        # if there is no custom menu, then there should be a valid page argument; see if it has children
+        candidates = page.get_children()
+
+        # if so, create a list of all items that have show_in_menus == True
+        menu_items = []
+        for candidate in candidates:
+            if candidate.show_in_menus:
+                menu_items.append({'title': candidate.title, 'url': candidate.url,
+                                   'slug': None, 'page': candidate, 'icon': None})
+        return menu_items
+    except AttributeError:
+        # neither custom menu nor valid page argument; return None
+        return None
