27b4b963e59f95f7501ecf41b296163946784040
paulrjmsmits <paulrjmsmits@users.noreply.github.com>
2020-07-29 17:07:01 +0200

    revert previous commit

--- pet/settings/production.py
+++ pet/settings/production.py
@@ -3,11 +3,6 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['pythoneatstail.com', 'www.pythoneatstail.com',]
 
-# ManifestStaticFilesStorage is recommended in production, to prevent outdated
-# Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
 try:
     from .local import *
 except ImportError:
